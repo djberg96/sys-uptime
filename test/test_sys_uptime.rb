b522bb982a57558bb3e9d67c346e19bfd704cd19
Daniel Berger <djberg96@gmail.com>
2011-12-10 07:27:10 -0700

    Updated test suite, and it now uses test-unit 2.

--- test/test_sys_uptime.rb
+++ test/test_sys_uptime.rb
@@ -1,65 +1,96 @@
 #####################################################################
-# tc_uptime.rb
+# test_sys_uptime.rb
 #
 # Test suite for sys-uptime. This should generally be run via the
 # 'rake test' task, since it handles the pre-setup code for you.
 #####################################################################
+require 'rubygems'
+gem 'test-unit'
+
 require 'sys/uptime'
 require 'test/unit'
 include Sys
 
-class TC_Uptime < Test::Unit::TestCase
-   def test_version
-      assert_equal('0.6.0', Uptime::VERSION)
-   end
+class TC_Sys_Uptime < Test::Unit::TestCase
+  test "version is set to expected value" do
+    assert_equal('0.6.0', Uptime::VERSION)
+  end
 
-   def test_seconds
-      assert_respond_to(Uptime, :seconds)
-      assert_nothing_raised{ Uptime.seconds }
-      assert_kind_of(Fixnum, Uptime.seconds)
-      assert_equal(true, Uptime.seconds > 0)
-   end
+  test "seconds method basic functionality" do
+    assert_respond_to(Uptime, :seconds)
+    assert_nothing_raised{ Uptime.seconds }
+  end
 
-   def test_minutes
-      assert_respond_to(Uptime, :minutes)
-      assert_nothing_raised{ Uptime.minutes }
-      assert_kind_of(Fixnum, Uptime.minutes)
-   end
+  test "seconds method returns a plausible value" do
+    assert_kind_of(Integer, Uptime.seconds)
+    assert_true(Uptime.seconds > 300)
+  end
 
-   def test_hours
-      assert_respond_to(Uptime, :hours)
-      assert_nothing_raised{ Uptime.hours }
-      assert_kind_of(Fixnum, Uptime.hours)
-   end
+  test "minutes method basic functionality" do
+    assert_respond_to(Uptime, :minutes)
+    assert_nothing_raised{ Uptime.minutes }
+  end
 
-   def test_days
-      assert_respond_to(Uptime,:days)
-      assert_nothing_raised{ Uptime.days }
-      assert_kind_of(Fixnum, Uptime.days)
-   end 
+  test "minutes method returns a plausible value" do
+    assert_kind_of(Integer, Uptime.minutes)
+    assert_true(Uptime.minutes > 5)
+  end
 
-   def test_uptime
-      assert_respond_to(Uptime,:uptime)
-      assert_nothing_raised{ Uptime.uptime }
-      assert_kind_of(String, Uptime.uptime)
-      assert_equal(false, Uptime.uptime.empty?)
-   end
-   
-   def test_dhms
-      assert_respond_to(Uptime,:dhms)
-      assert_nothing_raised{ Uptime.dhms }
-      assert_kind_of(Array, Uptime.dhms)
-      assert_equal(false, Uptime.dhms.empty?)
-      assert_equal(4, Uptime.dhms.length)
-   end
-   
-   def test_boot_time
-      assert_respond_to(Uptime,:boot_time)
-      assert_nothing_raised{ Uptime.boot_time }
-      assert_kind_of(Time, Uptime.boot_time)
-   end
+  test "hours method basic functionality" do
+    assert_respond_to(Uptime, :hours)
+    assert_nothing_raised{ Uptime.hours }
+  end
 
-   def test_uptime_error
-      assert_kind_of(StandardError, Uptime::Error.new)
-   end
+  test "hours method returns a plausible value" do
+    assert_kind_of(Integer, Uptime.hours)
+    assert_true(Uptime.hours > 0)
+  end
+
+  test "days method basic functionality" do
+    assert_respond_to(Uptime,:days)
+    assert_nothing_raised{ Uptime.days }
+  end
+
+  test "days method returns a plausible value" do
+    assert_kind_of(Fixnum, Uptime.days)
+    assert_true(Uptime.days >= 0)
+  end
+
+  test "uptime method basic functionality" do
+    assert_respond_to(Uptime,:uptime)
+    assert_nothing_raised{ Uptime.uptime }
+  end
+
+  test "uptime method returns a non-empty string" do
+    assert_kind_of(String, Uptime.uptime)
+    assert_false(Uptime.uptime.empty?)
+  end
+
+  test "uptime method does not accept any arguments" do
+    assert_raise(ArgumentError){ Uptime.uptime(1) }
+  end
+
+  test "dhms method basic functionality" do
+    assert_respond_to(Uptime,:dhms)
+    assert_nothing_raised{ Uptime.dhms }
+    assert_kind_of(Array, Uptime.dhms)
+  end
+
+  test "dhms method returns an array of four elements" do
+    assert_false(Uptime.dhms.empty?)
+    assert_equal(4, Uptime.dhms.length)
+  end
+
+  test "boot_time method basic functionality" do
+    assert_respond_to(Uptime,:boot_time)
+    assert_nothing_raised{ Uptime.boot_time }
+  end
+
+  test "boot_time method returns a Time object" do
+    assert_kind_of(Time, Uptime.boot_time)
+  end
+
+  test "Uptime class cannot be instantiated" do
+    assert_kind_of(StandardError, Uptime::Error.new)
+  end
 end
