0d5416996643320535628191ac455010a619cb0f
Daniel Berger <dberger@redhat.com>
2016-05-14 08:12:14 -0600

    Initial commit. Contains VERSION used by all platforms.

---
+++ lib/sys/uptime.rb
@@ -0,0 +1,12 @@
+if File::ALT_SEPARATOR
+  require_relative 'windows/sys/uptime'
+else
+  require_relative 'unix/sys/uptime'
+end
+
+module Sys
+  class Uptime
+    # The version of the sys-uptime library
+    VERSION = '0.7.1'
+  end
+end
